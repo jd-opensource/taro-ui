9d3e7a1759aee833f33cf35ff2e43a25928ddb00
heyongqi10 <heyongqi10@jd.com>
2026-05-09 14:53:06 +0800

    feat: 更新打包地址

--- docs/build/build-static.js
+++ docs/build/build-static.js
@@ -11,7 +11,7 @@ const distRoot = path.resolve(__dirname, '../.vitepress/dist')
 fs.emptyDirSync(path.resolve(distRoot, 'h5'))
 
 function mergeDemoH5AndDist() {
-  const demoH5Path = path.resolve(__dirname, '../../taro-ui-demo/dist')
+  const demoH5Path = path.resolve(__dirname, '../taro-ui-demo/dist')
   if (!fs.existsSync(demoH5Path)) {
     console.log('Please run `npm run build:demo` first')
     spinner.stop()
