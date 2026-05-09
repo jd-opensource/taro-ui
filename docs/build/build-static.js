0cb81b171cfd3bc0682eafbcc055ae28296fae81
Pixsway <pixsway@foxmail.com>
2026-05-09 17:16:44 +0800

    feat(docs): 迁移文档站 packages/taro-ui-docs → docs/ (#1858)
    * feat: 拷贝docs文件目录
    
    * refactor(docs): 将文档目录从packages/taro-ui-docs迁移至docs
    
    * chore(docs): 重构文档模块依赖并更新pnpm-lock.yaml
    
    * feat: 处理样式引入问题
    
    * feat: 更新打包地址
    
    * feat: 拷贝资源文件
    
    * feat: 修改文件路径
    
    * feat: 删除过时的 examples/about-sass-version
    
    * feat: 复制两个demo
    
    * feat: 复制两个demo
    
    * feat: 修改部分名称
    
    * feat: 更改文件名称
    
    * feat: 更改脚本名称
    
    * feat: 更改demo的名称
    
    * feat: 更新lock文件
    
    * chore(项目结构): 统一将demo目录迁移至examples目录
    
    * feat: 更新文档站名称
    
    * feat: 更新npm+lock文件

--- docs/build/build-static.js
+++ docs/build/build-static.js
@@ -6,12 +6,12 @@ const spinner = ora('Copy h5 website to docs...')
 
 spinner.start()
 
-// packages/taro-ui-docs/.vitepress/dist/h5
+// docs/.vitepress/dist/h5
 const distRoot = path.resolve(__dirname, '../.vitepress/dist')
 fs.emptyDirSync(path.resolve(distRoot, 'h5'))
 
 function mergeDemoH5AndDist() {
-  const demoH5Path = path.resolve(__dirname, '../../taro-ui-demo/dist')
+  const demoH5Path = path.resolve(__dirname, '../../examples/demo/dist')
   if (!fs.existsSync(demoH5Path)) {
     console.log('Please run `npm run build:demo` first')
     spinner.stop()
@@ -20,7 +20,7 @@ function mergeDemoH5AndDist() {
 
   if (!fs.existsSync(path.resolve(demoH5Path, 'index.html'))) {
     console.log(
-      'H5 demo index.html not found. Please run `pnpm --filter taro-ui-demo run build:h5` first'
+      'H5 demo index.html not found. Please run `pnpm --filter demo run build:h5` first'
     )
     spinner.stop()
     return
