c5012dce70972f368b91b15d729d7e7ab0897741
heyongqi10 <heyongqi10@jd.com>
2026-05-25 12:15:24 +0800

    refactor(loading): convert AtLoading component from class to functional component
    - Changed AtLoading from a class-based component to a functional component for improved readability and performance.
    - Removed defaultProps and integrated default values directly in the function parameters.
    - Simplified the render logic while maintaining existing functionality.

--- packages/taro-ui/src/components/loading/index.tsx
+++ packages/taro-ui/src/components/loading/index.tsx
@@ -1,4 +1,4 @@
-import PropTypes, { InferProps } from 'prop-types'
+import PropTypes from 'prop-types'
 import React from 'react'
 import { View } from '@tarojs/components'
 import { pxTransform } from '../../common/utils'
@@ -8,39 +8,30 @@ interface AtLoadingProps {
   color?: string | number
 }
 
-export default class AtLoading extends React.Component<AtLoadingProps> {
-  public static defaultProps: AtLoadingProps
-  public static propTypes: InferProps<AtLoadingProps>
-
-  public render(): JSX.Element {
-    const { color, size } = this.props
-    const loadingSize = typeof size === 'string' ? size : String(size)
-    const sizeStyle = {
-      width: size ? `${pxTransform(parseInt(loadingSize))}` : '',
-      height: size ? `${pxTransform(parseInt(loadingSize))}` : ''
-    }
-    const colorStyle = {
-      border: color ? `1px solid ${color}` : '',
-      borderColor: color ? `${color} transparent transparent transparent` : ''
-    }
-    const ringStyle = Object.assign({}, colorStyle, sizeStyle)
-
-    return (
-      <View className='at-loading' style={sizeStyle}>
-        <View className='at-loading__ring' style={ringStyle}></View>
-        <View className='at-loading__ring' style={ringStyle}></View>
-        <View className='at-loading__ring' style={ringStyle}></View>
-      </View>
-    )
+function AtLoading({ color = '', size = 0 }: AtLoadingProps): JSX.Element {
+  const loadingSize = typeof size === 'string' ? size : String(size)
+  const sizeStyle = {
+    width: size ? `${pxTransform(parseInt(loadingSize))}` : '',
+    height: size ? `${pxTransform(parseInt(loadingSize))}` : ''
   }
-}
+  const colorStyle = {
+    border: color ? `1px solid ${color}` : '',
+    borderColor: color ? `${color} transparent transparent transparent` : ''
+  }
+  const ringStyle = Object.assign({}, colorStyle, sizeStyle)
 
-AtLoading.defaultProps = {
-  size: 0,
-  color: ''
+  return (
+    <View className='at-loading' style={sizeStyle}>
+      <View className='at-loading__ring' style={ringStyle}></View>
+      <View className='at-loading__ring' style={ringStyle}></View>
+      <View className='at-loading__ring' style={ringStyle}></View>
+    </View>
+  )
 }
 
 AtLoading.propTypes = {
   size: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   color: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
 }
+
+export default AtLoading
